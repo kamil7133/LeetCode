a4c1bdc2995ac993280751e28a83c45e922729bb
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-04 06:36:33 +0100

    /Users/kamilpiwowarczyk/Python/pythonProject/LeetCode/2-DDP

---
+++ 2-DDP/longestcommonsubsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        m, n = len(text1), len(text2)
+        @cache
+        def longest(i, j):
+            if i == m or j == n:
+                return 0
+            elif text1[i] == text2[j]:
+                return 1 + longest(i + 1, j + 1)
+            else:
+                return max(longest(i, j + 1), longest(i + 1, j))
+
+
+        return longest(0, 0)
+
+
+    #🤷‍♀️
