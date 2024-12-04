8b4bebbed55ae8de1452f171327da26a6c2ca25a
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-04 06:39:14 +0100

    https://leetcode.com/problems/unique-paths/description/

--- 2-DDP/uniquepaths.py
+++ 2-DDP/uniquepaths.py
@@ -1,12 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = []
-
         for _ in range(m):
             dp.append([0] * n)
-
         dp[0][0] = 1
-
         for i in range(m):
             for j in range(n):
                 if i == j == 0:
@@ -18,4 +15,4 @@ class Solution:
                     val += dp[i][j - 1]
                 dp[i][j] = val
 
-        return dp[m-1][n-1]
+        return dp[m-1][n-1] #.
